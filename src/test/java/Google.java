8dd2d93d6bb291ac3e504d7c5559e1710592b845
Tester <andrewgiftson@gmail.com>
2021-11-17 16:24:32 +0530

    yaml file

--- src/test/java/Google.java
+++ src/test/java/Google.java
@@ -3,24 +3,29 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Google {
     public WebDriver driver;
    @BeforeTest
- public void setup(){
-     System.out.println("User directory is - "+System.getProperty("user.dir"));
-    System.setProperty("webdriver.chrome.driver", "/home/circleci/project/src/test/resources/chromedriver.exe");
-   // System.setProperty("webdriver.chrome.driver", "src//test//resources//chromedriver.exe");
-   // System.setProperty("webdriver.chrome.driver", "chromedriver");
-      // ChromeOptions options = new ChromeOptions();
-     driver=new ChromeDriver();
-
-     driver.manage().window().maximize();
-     driver.get("https://www.google.co.in/");
+ public void setup() throws MalformedURLException {
+//     System.out.println("User directory is - "+System.getProperty("user.dir"));
+//    System.setProperty("webdriver.chrome.driver", "/home/circleci/project/src/test/resources/chromedriver.exe");
+//
+//     driver=new ChromeDriver();
+//
+//     driver.manage().window().maximize();
+//     driver.get("https://www.google.co.in/");
+       DesiredCapabilities capabilities = new DesiredCapabilities();
+       capabilities.setCapability("browserName", "chrome");
    }
    @Test
     public void testGoogleSearch(){
